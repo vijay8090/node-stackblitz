01652462c3f58622a7fccda9c0322cd8279959e6
vijay8090 <vijayakumar.anbu1@gmail.com>
2021-11-12 10:47:34 +0530

    2 files modified, 1 added

---
+++ data/departments.js
@@ -0,0 +1,18 @@
+const departments = [
+  {
+    name: 'dept1',
+    employees: ['1', '2', '3'],
+    transactions: [100, -50, 250, -20, 10],
+  },
+  {
+    name: 'dept2',
+    employees: ['4', '5', '6'],
+    transactions: [200, -150, 50, -10, 70],
+  },
+  {
+    name: 'dept3',
+    employees: ['7', '8', '9'],
+    transactions: [300, -50, 150, -110, 90],
+  },
+];
+module.exports = departments;
